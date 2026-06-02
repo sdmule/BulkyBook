5bb031e73e09207849a8e04cfdf3a9c68193c5b2
sdmule <mulesaurabh45@gmail.com>
2026-06-02 18:10:14 +0530

    Changed primary key of Category table and updated database

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -4,8 +4,8 @@ namespace BulkyBookWeb.Models
 {
     public class Category
     {
-        [Key]
-        public int CategoryIdentification { get; set; }
+        //[Key]
+        public int Id { get; set; }
         public string Name { get; set; }=string.Empty;
     }
 }
