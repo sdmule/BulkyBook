d962b9e1545eacc5aa7d4a126281cb009609dccd
sdmule <mulesaurabh45@gmail.com>
2026-06-02 18:22:09 +0530

    Seeded Category table

--- BulkyBookWeb/Data/ApplicationDbContext.cs
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -8,5 +8,17 @@ namespace BulkyBookWeb.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1, Name="Action"},
+                new Category { Id=2, Name="SciFi"},
+                new Category { Id=3, Name="History"}
+                );
+        }
+
     }
 }
